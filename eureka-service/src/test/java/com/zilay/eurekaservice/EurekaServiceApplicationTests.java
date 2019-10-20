f466a6e17982f247f12b60634af3e4826dff3d8e
zilehuda <zilehuda.tariq@gmail.com>
2019-10-20 17:05:15 +0500

    Netflix Eureka service on port 8761

---
+++ eureka-service/src/test/java/com/zilay/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zilay.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
